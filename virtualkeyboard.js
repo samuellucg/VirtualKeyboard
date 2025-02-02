08e3fe4ce4227eb418a551907f823596d373fd98
Samuel Lucas <samuel.lucasmacedos@gmail.com>
2025-02-02 18:23:27 -0300

    added function to drag using touch and a video to wallpaper on website (maybe we change video)

--- virtualkeyboard.js
+++ virtualkeyboard.js
@@ -1,16 +1,26 @@
-// virtual keyboard 
-window.addEventListener("resize", function() {
+// virtual keyboard code
+
+
+window.addEventListener("resize", function() { // Para cada redimensionamento o teclado será fechado caso esteja aberto.
     closeKeyboard();
 })
+
+
 const keyboard = document.getElementById("keyboardmod")
 const searchbar = document.getElementById("searchbar");
 let searchbarInput = document.getElementById("search-0");
 let modal;
+let offsetX = 0, offsetY = 0, isTouch = false;
 
 
-
-function openKeyboard() {
+function openKeyboard() { // To open keyboard and let draggable.
     modal = $("#keyboardmod")
+
+    // unificar jquery.
+
+
+    // Adicionar aria-hidden e role="dialog" ao teclado para melhorar a acessibilidade.
+
     if (keyboard.style.display != "flex") {
         keyboard.style.display = "flex";
         searchbar.style.display = "flex";
@@ -26,18 +36,52 @@ function openKeyboard() {
                 $("body").css("user-select", "");
             }
         });
+
+        keyboard.addEventListener("pointerdown",startTouch);
     }
     else{
         alert("O teclado já está aberto, para fechar. Clique no botão X")
+        // toast ou snackbar
     }
 }
 
-function setPosition() {
-    if (!modal) return {left: 0, maxLeft: 0, top: 0, maxTop: 0}
+function startTouch(e) { // pointerdown function
+    if (e.pointerType !== "touch") return ;
+
+    isTouch = true;
+    const kbdim = keyboard.getBoundingClientRect();
+    offsetX = e.clientX - kbdim.left;
+    offsetY = e.clientY - kbdim.top;
+
+    document.addEventListener("pointermove", touchMove);
+    document.addEventListener("pointerup", touchEnd);
+}
+
+function touchMove(e) { // pointermove function
+    if (!isTouch) return;
+
+    let newX = e.clientX - offsetX
+    let newY = e.clientY - offsetY
+
+    const maxX = window.innerWidth - keyboard.offsetWidth;
+    const maxY = window.innerHeight - keyboard.offsetHeight;
+
+    newX = Math.max(0, Math.min(newX, maxX))
+    newY = Math.max(0, Math.min(newY, maxY))
+
+    keyboard.style.left = `${newX}px`;
+    keyboard.style.top = `${newY}px`;
+}
+
+function touchEnd() { // pointerup function
+    isTouch = false;
+
+    document.removeEventListener("pointermove",touchMove)
+    document.removeEventListener("pointerup",touchEnd)
 }
 
 
-function closeKeyboard(){
+function closeKeyboard(){ // Just close keyboard.
     if(keyboard.style.display != "none"){
         keyboard.style.display = "none";
         searchbar.style.display = "none";
@@ -45,14 +89,14 @@ function closeKeyboard(){
     }
 }
 
-function typeKey(val) {
+function typeKey(val) { // LOL, you need to read to know his funcionality?
     searchbarInput.value += val
 }
 
-function clearSearch() {
+function clearSearch() { // Bro, you kidding me?
     searchbarInput.value = ""
 }
 
-function backspace() {
-    searchbarInput.value = searchbar.value.slice(0,-1)
+function backspace() { // I give up.
+    searchbarInput.value = searchbarInput.value.slice(0,-1)
 }
